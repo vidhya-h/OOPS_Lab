25e94e20b32f8fc7a36d2dc2006038c0319442a4
vidhya-h <141317308+vidhya-h@users.noreply.github.com>
2023-08-03 15:26:07 +0530

    Create inline.cpp

---
+++ inline.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+inline float lar(float a, float b ,float c=50 )
+{
+     return ((a>b&&a>c)?a:b>c?b:c);
+}
+int main()
+{
+    float x,y,z;
+    cout<<"\nEnter three numbers:\n";
+    cin>>x>>y>>z;
+    cout<<"Largest= "<<lar(x,y,z);
+    cout<<"\nTaking 3rd number as 50\n";
+    cout<<"\nEnter two numbers:\n";
+    cin>>x>>y;
+    cout<<"\nLargest using default value= "<<lar(x,y)<<"\n";
+    return 0;
+}
